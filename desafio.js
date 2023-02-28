10ba6938f3df3f1046583ff9e2e54a726296ef8f
Ignacio Ramos <ignaciobenjamin.ramos@gmail.com>
2023-02-28 16:35:16 -0300

    se modificaron los metodos addProduct,getproductById y se agregaron los metodos deleteProduct y updateProduct para que funcionen con archivos

--- desafio.js
+++ desafio.js
@@ -1,52 +1,99 @@
+import fs from "fs";
+
+
+
+
 class ProductManager{
     constructor(){
-        this.products = []
+        this.path = "./files/products"
+    }
+
+    getProducts = async()=>{
+        if (fs.existsSync(this.path)){
+            const data = await fs.promises.readFile(this.path,"utf-8")
+            const result = JSON.parse(data)
+            return result
+        }
+        else{
+            return []
+        }
     }
 
 
-    addProduct=(title,description,price,thumbnail,code,stock)=>{
-        const codeExist = this.products.find((event)=>event.code === code)
+    addProduct=async(product)=>{
+        const products = await this.getProducts()
+        if(products.length === 0){
+            product.id=1
+        }else{
+            product.id = products[products.length-1].id+1;
+        }
+        products.push(product);
 
-        if(!title || !description || !price || !thumbnail || !code || !stock){
-            console.log('Error: all fields are mandatory')
-            return
-        }
-        if(codeExist){
-            console.log(`Error: the code ${code} for the product already exists`)
-            return
-        }
-        const product={
-            id:this.products.length+1,
-            title,
-            description,
-            price,
-            thumbnail,
-            code,
-            stock
-        }
-        this.products.push(product)
+        await fs.promises.writeFile(this.path,JSON.stringify(products,null,"\t"))
     }
 
-    getProducts=()=>{
-        console.log(this.products)
-        return
-    }
-
-    getProductById=(productId)=>{
-        const event = this.products.find((product)=>product.id ===productId)
+    getProductsById = async (productId)=>{
+        const products = await this.getProducts()
+        const event = products.find((product)=>product.id=== productId)
         if(!event){
-            console.log('Product Not found')
-            return
+            return "Not found"
         }
-        console.log(event)
-        return
+        return event
+    }
+
+    deleteProduct = async (productId)=>{
+        const products = await this.getProducts()
+        const result = await products.filter(product=> product.id !== productId)
+        await fs.promises.writeFile(this.path,JSON.stringify(result,null,"\t"))
+    } 
+
+    updateProduct = async (productId,title,description,price,thumbnail,code,stock)=>{
+        const products = await this.getProducts()
+        const event = await products.find((product)=>product.id === productId)
+        const position =  products.indexOf(event)
+        const productoEditado={
+            ...event,
+            title: title ||event.title,
+            description: description || event.description,
+            price: price || event.price,
+            thumbnail: thumbnail || event.thumbnail,
+            code: code|| event.code,
+            stock: stock || event.stock,
+            id: event.id
+        }
+        products[position] = productoEditado
+        await fs.promises.writeFile(this.path,JSON.stringify(products,null,"\t"))
     }
 }
 
 
-const productManager = new ProductManager();
-productManager.addProduct('Cualquiera','el mejor',120,'image',220002,2)
-productManager.addProduct('Cualquiera','el mejor',120,'image',220002,2)
-productManager.addProduct('Ignacio','los mejors',120,'image')
-productManager.getProducts()
-// productManager.getProductById(3)
+
+
+const productManager = new ProductManager()
+
+const env= async()=>{
+
+    // console.log(await productManager.getProducts())
+    
+    
+    const product = {
+        title:"mancuerna",
+        description:"fuerte",
+        price: 100,
+        thumbnail:'asdasd',
+        code: 400,
+        stock:10
+    }
+    
+    //await productManager.addProduct(product)
+    
+    //console.log(await productManager.getProducts())
+    //console.log(await productManager.getProductsById(2))
+    
+    await productManager.deleteProduct(3)
+    
+    //console.log(await productManager.updateProduct(2,"Nuevo titulo",'Espero que funcione',300))
+    console.log( await productManager.getProducts())
+}
+
+env()
