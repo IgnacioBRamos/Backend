ed856bdfc119d9b88eb14e48233da853636979d6
Ignacio Ramos <ignaciobenjamin.ramos@gmail.com>
2023-08-21 15:54:39 -0300

    correcion ruta vencida de stripe

--- src/public/js/payment.js
+++ src/public/js/payment.js
@@ -4,6 +4,7 @@ button.addEventListener("click",async (e)=>{
     const res = await fetch("/api/payment",{
         method: "POST"
     })
-    const data = await res.json()
-    window.location.href="https://checkout.stripe.com/c/pay/cs_test_b15ZIAht4Se2IOc7Zy11x5JswsEQ5fsUwrMdYIQQriX7nuD7ZAt9wvgA3i#fidkdWxOYHwnPyd1blpxYHZxWjA0S2Jmf1VCY2ZMU3dmQzBPRFVMPF9iTnRCV2NyQktuZzJMf0ZDRDdRXFd1R21LalBzfDxVfGZhaGpBTGYyV3BhQ081Ul9wMUY8MlAxRG9DdE0zcGljSmMwNTVwVHZ9QWFnYScpJ2N3amhWYHdzYHcnP3F3cGApJ2lkfGpwcVF8dWAnPydocGlxbFpscWBoJyknYGtkZ2lgVWlkZmBtamlhYHd2Jz9xd3BgeCUl"
+    let data = await res.json()
+    
+    window.location.href= data.url
 })
