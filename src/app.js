bae00f541b1115447d27c739f5b2a230308aa58a
Ignacio Ramos <ignaciobenjamin.ramos@gmail.com>
2023-08-20 23:55:01 -0300

    Se corrigieron errores de rutas

--- src/app.js
+++ src/app.js
@@ -23,8 +23,8 @@ import cors from "cors"
 
 
 const app = express();
-app.use(cors())
 const PORT = process.env.PORT || 8080
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use("/", express.static(`${__dirname}/public`));
